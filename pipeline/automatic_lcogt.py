b1a104e7d6c95335762034c03b13a811c0c10daf
Melissa Hobson <melihobson@gmail.com>
2020-02-04 10:55:16 -0300

    fixed mixed tab and spaces in indentation

--- pipeline/automatic_lcogt.py
+++ pipeline/automatic_lcogt.py
@@ -94,74 +94,74 @@ def read_setupfile():
            emailreceiver,astrometry,gfastrometry
 
 class Bimail:
-	def __init__(self,subject,recipients):
-                fpack_folder,astrometry_folder,sendemail,emailsender,emailsender_pwd,\
-                             emailreceiver,astrometry,gfastrometry = read_setupfile()
-		self.subject = subject
-		self.recipients = recipients
-		self.htmlbody = ''
-		self.sender = emailsender 
-		self.senderpass = emailsender_pwd
-		self.attachments = []
+    def __init__(self,subject,recipients):
+        fpack_folder,astrometry_folder,sendemail,emailsender,emailsender_pwd,\
+                     emailreceiver,astrometry,gfastrometry = read_setupfile()
+        self.subject = subject
+        self.recipients = recipients
+        self.htmlbody = ''
+        self.sender = emailsender 
+        self.senderpass = emailsender_pwd
+        self.attachments = []
  
-	def send(self):
-		msg = MIMEMultipart('alternative')
-		msg['From']=self.sender
-		msg['Subject']=self.subject
-		msg['To'] = ", ".join(self.recipients) # to must be array of the form ['mailsender135@gmail.com']
-		msg.preamble = "preamble goes here"
-		#check if there are attachments if yes, add them
-		if self.attachments:
-			self.attach(msg)
-		#add html body after attachments
-		msg.attach(MIMEText(self.htmlbody, 'html'))
-		#send
-		s = smtplib.SMTP('smtp.gmail.com:587')
-		s.starttls()
-		s.login(self.sender,self.senderpass)
-		s.sendmail(self.sender, self.recipients, msg.as_string())
-		#test
-		print msg
-		s.quit()
-	
-	def htmladd(self, html):
-		self.htmlbody = self.htmlbody+'<p></p>'+html
+    def send(self):
+        msg = MIMEMultipart('alternative')
+        msg['From']=self.sender
+        msg['Subject']=self.subject
+        msg['To'] = ", ".join(self.recipients) # to must be array of the form ['mailsender135@gmail.com']
+        msg.preamble = "preamble goes here"
+        #check if there are attachments if yes, add them
+        if self.attachments:
+            self.attach(msg)
+        #add html body after attachments
+        msg.attach(MIMEText(self.htmlbody, 'html'))
+        #send
+        s = smtplib.SMTP('smtp.gmail.com:587')
+        s.starttls()
+        s.login(self.sender,self.senderpass)
+        s.sendmail(self.sender, self.recipients, msg.as_string())
+        #test
+        print msg
+        s.quit()
+    
+    def htmladd(self, html):
+        self.htmlbody = self.htmlbody+'<p></p>'+html
  
-	def attach(self,msg):
-                print self.attachments
-		for f in self.attachments:
-		
-			ctype, encoding = mimetypes.guess_type(f)
-			if ctype is None or encoding is not None:
-				ctype = "application/octet-stream"
-				
-			maintype, subtype = ctype.split("/", 1)
+    def attach(self,msg):
+        print self.attachments
+        for f in self.attachments:
+        
+            ctype, encoding = mimetypes.guess_type(f)
+            if ctype is None or encoding is not None:
+                ctype = "application/octet-stream"
+                
+            maintype, subtype = ctype.split("/", 1)
  
-			if maintype == "text":
-				fp = open(f)
-				# Note: we should handle calculating the charset
-				attachment = MIMEText(fp.read(), _subtype=subtype)
-				fp.close()
-			elif maintype == "image":
-				fp = open(f, "rb")
-				attachment = MIMEImage(fp.read(), _subtype=subtype)
-				fp.close()
-			elif maintype == "audio":
-				fp = open(f, "rb")
-				attachment = MIMEAudio(fp.read(), _subtype=subtype)
-				fp.close()
-			else:
-				fp = open(f, "rb")
-				attachment = MIMEBase(maintype, subtype)
-				attachment.set_payload(fp.read())
-				fp.close()
-				encoders.encode_base64(attachment)
-			attachment.add_header("Content-Disposition", "attachment", filename=f.split('/')[-1])
-			attachment.add_header('Content-ID', '<{0}>'.format(f.split('/')[-1]))
-			msg.attach(attachment)
-	
-	def addattach(self, files):
-		self.attachments = self.attachments + files
+            if maintype == "text":
+                fp = open(f)
+                # Note: we should handle calculating the charset
+                attachment = MIMEText(fp.read(), _subtype=subtype)
+                fp.close()
+            elif maintype == "image":
+                fp = open(f, "rb")
+                attachment = MIMEImage(fp.read(), _subtype=subtype)
+                fp.close()
+            elif maintype == "audio":
+                fp = open(f, "rb")
+                attachment = MIMEAudio(fp.read(), _subtype=subtype)
+                fp.close()
+            else:
+                fp = open(f, "rb")
+                attachment = MIMEBase(maintype, subtype)
+                attachment.set_payload(fp.read())
+                fp.close()
+                encoders.encode_base64(attachment)
+            attachment.add_header("Content-Disposition", "attachment", filename=f.split('/')[-1])
+            attachment.add_header('Content-ID', '<{0}>'.format(f.split('/')[-1]))
+            msg.attach(attachment)
+    
+    def addattach(self, files):
+        self.attachments = self.attachments + files
 
 def datesok(year,month):
     if year == 2016:
